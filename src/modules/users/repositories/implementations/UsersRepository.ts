fc3b5f082ba1a9e79c723dbe05e4905d988474e2
Rychell Sousa <rychell@gmail.com>
2021-05-04 20:18:27 -0300

    All methods finished

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -10,21 +10,28 @@ export class UsersRepository implements IUsersRepository {
   constructor() {
     this.repository = getRepository(User);
   }
-
+  
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = this.repository.findOneOrFail({
+      relations:['games'],
+      where:{id: user_id}
+    })
+
+    return user
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users ORDER BY first_name ASC`); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+
+    return this.repository.query(`SELECT * FROM users WHERE first_name=INITCAP($1) AND last_name=INITCAP($2)`, [first_name.toLowerCase(), last_name.toLowerCase()]); // Complete usando raw query
   }
 }
